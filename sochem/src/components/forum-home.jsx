cb8ebd5fb5c9cd372190d1531adf22c6360e81a0
aman909909 <amanashish909@gmail.com>
2020-10-12 21:01:29 +0530

    Add Router

---
+++ sochem/src/components/forum-home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+function Forum(){
+
+    return (
+        <div>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+}
+
+export default Forum;
+
--- sochem/src/index.js
+++ sochem/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Forum from './components/forum-home'
+
+const routing = (
+  <BrowserRouter>
+      <Route exact path="/forum" component={Forum}/>
+  </BrowserRouter>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
