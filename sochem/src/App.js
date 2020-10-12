f6b799019f5944f0b4c30b8b3b2e40b5a430294c
aman909909 <amanashish909@gmail.com>
2020-10-12 20:47:22 +0530

    Add bootstrap

--- sochem/src/App.js
+++ sochem/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className=" container jumbotron bg-light">
       <header className="App-header">
         SoChem
       </header>
